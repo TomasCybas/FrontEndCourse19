5ed1c95ca1517617346aa5c13f53e7bad9b905a2
Tomas <tcybas@gmail.com>
2019-06-10 00:39:10 +0300

    Homework18
    added data output

--- homework20/js/scripts.js
+++ homework20/js/scripts.js
@@ -1,17 +1,26 @@
+'use strict';
+
 var carData = [];
 
-function Car(date, nr, s, t,){
+document.getElementById("cars_form").addEventListener("submit", function (e) {
+    e.preventDefault();
+    addCar();
+    $('#carsFormModal').modal('hide')
+    .on('hidden.bs.modal', function () { //resets modal form values
+        $(this).find('form').trigger('reset');
+    })
+});
+
+function Car(date, nr, s, t) {
     this.date = date;
     this.numberPlate = nr;
     this.travelDistance = s;
     this.travelTime = t;
-    this.speed = function () {
-        return this.travelDistance / this.travelTime * 3.6;
-    };
+    this.speed = (this.travelDistance / this.travelTime * 3.6).toFixed(2);
 }
 
-function addCar () {
-    var car = new Car (
+function addCar() {
+    let car = new Car(
         document.getElementById("date").value,
         document.getElementById("num_plate").value,
         document.getElementById("distance").value,
@@ -20,6 +29,30 @@ function addCar () {
     carData.push(car);
     console.log(carData);
     console.log(car);
+    displayData(carData)
 }
+
+function displayData(carData) {
+    let html = "";
+    let entryId = 0;
+    carData.forEach(function (element) {
+        html += '<tr ' + 'id=' + '"' + entryId++ + '"' + '>';
+        for (let i in element) {
+            if (element.hasOwnProperty(i)) {
+                html += '<td>' + element[i] + '</td>'
+            }
+        }
+        html += '<td>' + '<button type="button" class="btn btn-primary btn-sm">Redaguoti įrašą</button>' + '</td>' +
+            '<td>' + '<button type="button" class="btn btn-danger btn-sm">Ištrinti įrašą</button>' + '</td>' + '</tr>';
+    });
+    document.getElementById('cars_table').innerHTML = html;
+}
+
+
+
+
+
+
+
 
 
